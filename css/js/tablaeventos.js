ad790a3aaa2d682602c53756cb6456bee40f37e9
UxiaRodriguezCastro <uxiacastro@gmail.com>
2023-11-12 21:29:49 +0100

    arreglar tabla eventos

--- css/js/tablaeventos.js
+++ css/js/tablaeventos.js
@@ -42,7 +42,40 @@ fetch('css/jsons/eventos_esp.json') // Reemplaza 'ruta/del/json.json' con la rut
 
             // Agrega la fila al cuerpo de la tabla
             tablaBody.appendChild(fila);
+            document.getElementById('encabezado1').addEventListener('click', function () {
+                mostrarFilas('grupo1');
+                var columna = document.getElementById('encabezado1');
+                columna.classList.add('activo');
+                columna = document.getElementById('encabezado2');
+                columna.classList.remove('activo');
+                ocultarFilas('grupo2');
+            });
+            document.getElementById('encabezado1').click();
+            document.getElementById('encabezado2').addEventListener('click', function () {
+                mostrarFilas('grupo2');
+                ocultarFilas('grupo1');
+                var columna = document.getElementById('encabezado2');
+                columna.classList.add('activo');
+                columna = document.getElementById('encabezado1');
+                columna.classList.remove('activo');
+            });
 
+
+            function mostrarFilas(grupo) {
+                var filas = document.getElementsByClassName(grupo);
+
+                for (var i = 0; i < filas.length; i++) {
+                    filas[i].classList.remove('oculto');
+                }
+            }
+
+            function ocultarFilas(grupo) {
+                var filas = document.getElementsByClassName(grupo);
+
+                for (var i = 0; i < filas.length; i++) {
+                    filas[i].classList.add('oculto');
+                }
+            }
         });
     })
     .catch(error => console.error('Error al obtener el JSON:', error));
